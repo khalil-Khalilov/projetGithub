ae785f4d0805517c3dfa3a5ce6a7ea6504fdd692
prosperkouassi <74535748+prosperkouassi@users.noreply.github.com>
2020-12-09 16:24:18 +0100

    Add files via upload

--- portfolio.php
+++ portfolio.php
@@ -1,1 +1,85 @@
-<p>je suis la partie portfolio</p>
+
+    <section class="nav">
+    <div class="mini-nav"><h1 class="guy-h1">Portfolio</h1></div>
+  
+        <ul class="main-menu">
+            <li class="alone"><a href="#">ALL</a></li>
+            <li>Panorama</li>
+            <li>Portrait</li>
+            <li>Macros</li>
+            <li>Journal</li>
+        </ul>
+     
+</section>
+<div class="container-fluid">
+    
+</div>
+<article class="cards-portfolio">
+
+    <div class="all-cards">
+
+        <div class="card-1"></div>
+        <div class="card-2"></div>
+        <div class="card-3"></div>
+        <div class="card-4"></div>
+        <div class="card-5"></div>
+        <div class="card-6"></div>
+        <div class="card-7"></div>
+        <div class="card-8"></div>
+    </div>
+</div>
+</article>
+    
+<section>
+
+<div class="container setup">
+    <div class="service">
+        <h2>JUST DEFAUT SECTION</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci laudantium nisi delectus rem quasi explicabo expedita nulla quo officiis esse ab, ratione dolor exercitationem maiores laboriosam repellendus assumenda eaque. Fugit.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eos qui delectus cum ut incidunt eum exercitationem, reiciendis debitis nam ducimus unde saepe repellat temporibus quae labore. Aliquam, perspiciatis minus.</p>
+           <a href="#">VISIT ME</a>  
+    </div>
+    <div class="videos">
+       <iframe width="460" height="315" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ </div>
+</div>
+</section>
+
+<div class='container-fluid color'>
+        <div class='container'>
+           <div class='twitter_spacebetween'>
+               <h2>TWITTER</h2>
+                   <img src="images/twitter.png"/>
+        </div>
+           <hr class='twitter_hr'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum odio tempore ut id nostrum natus, accusamus deleniti magnam numquam.</p>
+             <p><span class='twitter_bold'>@Lorem ipsum dolor</span> sit amet consectetur adipisicing elit.</p>     
+        </div>
+ </div>
+
+
+
+
+
+
+
+
+
+<!--<section class="container-fluid color ">
+     <div class="container">
+
+        <div class="tweet">
+            <div class="mot"> 
+                 <h2>TWITTER</h2>
+            </div>
+        </div>
+
+        <div class="icons">
+            <img src="images/twitter.png" alt=""/>
+        </div>
+
+    </div>
+
+
+</section>-->
+ 
