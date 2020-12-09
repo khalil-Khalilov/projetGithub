88ab5974bcc3948dac02cc944afd1d8701e727ba
Khalil <64747026+khalil-Khalilov@users.noreply.github.com>
2020-12-09 16:20:28 +0100

    Add files via upload

--- services.php
+++ services.php
@@ -1,1 +1,45 @@
-<p>je suis la partie services</p>
+<div id="khalil-services_container">
+   <div id="khalil-h2"><h2>SERVICES</h2></div> 
+
+    <div id="khalil-inner_container">
+        <ul>
+            <li>
+                <img src="images/coffee">
+                <h3>Coffee</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </li>
+
+            <li>
+            <img src="images/instant">
+                <h3>Instant</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+                </p>
+            </li>
+
+            <li>
+            <img src="images/serious">
+                <h3>Serious</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </li>
+
+            <li>
+            <img src="images/frame">
+                <h3>Frame</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </li>
+        </ul>
+    </div>
+</div>
+
